40aee0ed37b50e34d1ead7eeaef18e0ba7315a1d
Aurélien Barre <aurelien.barre@parrot.com>
2017-05-07 21:27:00 +0200

    [DEV] add config files for pclinux and raspi

---
+++ config/raspi/product.mk
@@ -0,0 +1,19 @@
+
+CONFIG_DIR := $(call my-dir)
+
+TARGET_OS := linux
+TARGET_OS_FLAVOUR := generic
+TARGET_ARCH :=  arm
+TARGET_DEFAULT_ARM_MODE := arm
+TARGET_CROSS := /opt/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-
+TARGET_LIBC := eglibc
+TARGET_FLOAT_ABI := hard
+export SDKSTAGE := $(CONFIG_DIR)/../../../../../raspberrypi-sdk
+TARGET_GLOBAL_CFLAGS += -I$(SDKSTAGE)/usr/include
+TARGET_GLOBAL_LDLIBS += -L$(SDKSTAGE)/usr/lib
+
+ARSDK_BUILD_FOR_APP := 1
+
+# Use our own json, ncurses version
+prebuilt.json.override := 1
+prebuilt.ncurses.override := 1
