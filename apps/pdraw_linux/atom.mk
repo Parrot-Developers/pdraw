47686eeec5044e1a08d1d1782e067fbee606797f
Aurélien Barre <aurelien.barre@parrot.com>
2017-05-24 18:20:05 +0200

    [BUILD] pdraw_linux: use SDL as prebuilt dependency

--- apps/pdraw_linux/atom.mk
+++ apps/pdraw_linux/atom.mk
@@ -7,8 +7,7 @@ LOCAL_MODULE := pdraw_linux
 LOCAL_DESCRIPTION := Parrot Drones Awesome Video Viewer Linux Application
 LOCAL_CATEGORY_PATH := multimedia
 LOCAL_SRC_FILES := pdraw_linux.c
-LOCAL_LIBRARIES := libpdraw libulog libARCommands libARNetwork libARNetworkAL libARDiscovery json curl
-LOCAL_CFLAGS += $(shell sdl-config --cflags) -DUSE_SDL
-LOCAL_LDLIBS += $(shell sdl-config --libs)
+LOCAL_LIBRARIES := libpdraw libulog libARCommands libARNetwork libARNetworkAL libARDiscovery json curl sdl
+LOCAL_CFLAGS += -DUSE_SDL
 
 include $(BUILD_EXECUTABLE)
