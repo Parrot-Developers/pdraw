56485428bff92d26acf688609738a37c0ba50797
Aurélien Barre <aurelien.barre@parrot.com>
2017-06-03 17:03:50 +0200

    [BUILD] raspi: remove libpng dependency for libpdraw (unused)

--- libpdraw/atom.mk
+++ libpdraw/atom.mk
@@ -37,7 +37,7 @@ ifeq ("$(TARGET_OS)","linux")
 		LOCAL_LDLIBS += -lavcodec -lavdevice -lavfilter -lavformat -lswresample -lswscale -lavutil -lGL -lGLU
 	else ifeq ("$(TARGET_OS_FLAVOUR)","generic")
 		ifeq ("$(TARGET_PRODUCT_VARIANT)","raspi")
-			LOCAL_LIBRARIES += ilclient libpng
+			LOCAL_LIBRARIES += ilclient
 			LOCAL_CFLAGS += -DBCM_VIDEOCORE -DUSE_VIDEOCOREOMX -DUSE_VIDEOCOREEGL -DUSE_GLES2 -I$(SDKSTAGE)/opt/vc/include
 			LOCAL_CFLAGS += -DSTANDALONE -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DTARGET_POSIX -D_LINUX -fPIC -DPIC -D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -U_FORTIFY_SOURCE -Wall -g -DHAVE_LIBOPENMAX=2 -DOMX -DOMX_SKIP64BIT -ftree-vectorize -pipe -DUSE_EXTERNAL_OMX -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi
 			LOCAL_LDLIBS += -L$(SDKSTAGE)/opt/vc/lib -lbrcmGLESv2 -lbrcmEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm -lpthread -lrt -lm
