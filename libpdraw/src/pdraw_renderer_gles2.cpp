5a8ff089e1ad72d007b8c6686ad1ad082594cc82
Aurélien Barre <aurelien.barre@parrot.com>
2017-06-28 10:28:53 +0200

    [DEBUG] HMD distorsion correction: use linear interpolation for FBO

--- libpdraw/src/pdraw_renderer_gles2.cpp
+++ libpdraw/src/pdraw_renderer_gles2.cpp
@@ -162,8 +162,10 @@ int Gles2Renderer::initGles2()
         glBindTexture(GL_TEXTURE_2D, mFboTexture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mRenderWidth / 2, mRenderHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
         glGenRenderbuffers(1, &mFboRenderBuffer);
         if (mFboRenderBuffer <= 0)
