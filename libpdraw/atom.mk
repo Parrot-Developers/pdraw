e448795fdc4fbb7f0958ea9865e9771c85fe8300
Aurélien Barre <aurelien.barre@parrot.com>
2017-10-04 21:07:23 +0200

    [BUILD] move python binding to libpdraw/python
    Change-Id: I3f3729191db9c733dfe41a8755e86aa4c87fe9fd

--- libpdraw/atom.mk
+++ libpdraw/atom.mk
@@ -78,3 +78,58 @@ else ifeq ("$(TARGET_OS)-$(TARGET_OS_FLAVOUR)-$(TARGET_PRODUCT_VARIANT)","linux-
 endif
 
 include $(BUILD_LIBRARY)
+
+
+ifneq ("$(shell which python-config)","")
+ifneq ("$(shell which swig)","")
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := pdraw_python
+LOCAL_DESCRIPTION := PDrAW wrapper to python using SWIG
+LOCAL_CATEGORY_PATH := multimedia
+
+LOCAL_LIBRARIES := libpdraw
+LOCAL_CONFIG_FILES := python/aconfig.in
+$(call load-config)
+
+PDRAW_PYTHON_SWIG_WRAPPER := _pdraw_python.cpp
+PDRAW_PATH=$(LOCAL_PATH)
+
+LOCAL_C_INCLUDES := $(shell find $(PDRAW_PATH)/include $(PDRAW_PATH)/src -type d)
+
+PDRAW_PYTHON_SWIG_C_INCLUDES := $(foreach f, $(LOCAL_C_INCLUDES), -I$(f))
+
+LOCAL_GENERATED_SRC_FILES := $(PDRAW_PYTHON_SWIG_WRAPPER)
+LOCAL_MODULE_FILENAME := _$(LOCAL_MODULE).so
+LOCAL_DESTDIR := usr/lib/python
+
+PDRAW_PYTHON_DESTDIR := $(TARGET_OUT_STAGING)/usr/lib/python
+
+ifeq ($(CONFIG_PDRAW_PYTHON_PYTHON3),y)
+  PDRAW_PYTHON_PYTHONCONFIG := python3-config
+else
+  PDRAW_PYTHON_PYTHONCONFIG := python-config
+endif
+
+LOCAL_CXXFLAGS := -std=c++11
+LOCAL_CXXFLAGS += $(shell $(PDRAW_PYTHON_PYTHONCONFIG) --includes)
+
+LOCAL_LDLIBS := $(shell $(PDRAW_PYTHON_PYTHONCONFIG) --ldflags)
+
+PDRAW_PYTHON_SWIG_SOURCE := python/$(LOCAL_MODULE).i
+PDRAW_PYTHON_NAME := $(LOCAL_MODULE)
+
+PDRAW_PYTHON_SWIG_DEPENDS := $(foreach d, $(LOCAL_C_INCLUDES), $(d)/*) $(shell find $(LOCAL_PATH)/*i)
+
+$(LOCAL_BUILD_DIR)/$(PDRAW_PYTHON_SWIG_WRAPPER): $(LOCAL_PATH)/$(PDRAW_PYTHON_SWIG_SOURCE) $(PDRAW_PYTHON_SWIG_DEPENDS)
+	$(call print-banner1,"$(PRIVATE_MODE_MSG)Swig",$(PDRAW_PYTHON_SWIG_WRAPPER),$(PDRAW_PYTHON_SWIG_SOURCE))
+	$(Q) swig -python -c++ $(PDRAW_PYTHON_SWIG_C_INCLUDES) -o $@ $<
+	$(Q) mkdir -p $(PDRAW_PYTHON_DESTDIR)
+	$(Q) cp $(LOCAL_BUILD_DIR)/$(PDRAW_PYTHON_NAME).py $(PDRAW_PYTHON_DESTDIR)
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+endif
