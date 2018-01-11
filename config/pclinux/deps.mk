88bbf0e6a9dd62d937ce66ff7becc5814282e9ea
Aurélien Barre <aurelien.barre@parrot.com>
2018-01-11 23:33:44 +0100

    [CONFIG] pclinux: update the prebuilt deps
    Remove libavcodec and libavutil (we are building our own)
    
    Change-Id: Icf8dd63917667600fe2b3cca0b6a2f5ce6baa28d
    Reviewed-on: https://gerrit-doc.parrot.biz/84654
    Tested-by: Jenkins_dragon <jenkins.dragon@parrot.com>
    Reviewed-by: Aurelien Barre <aurelien.barre@parrot.com>

--- config/pclinux/deps.mk
+++ config/pclinux/deps.mk
@@ -4,7 +4,3 @@ $(call register-prebuilt-pkg-config-module,sdl2,sdl2)
 $(call register-prebuilt-pkg-config-module,gl,gl)
 
 $(call register-prebuilt-pkg-config-module,glfw3,glfw3)
-
-$(call register-prebuilt-pkg-config-module,libavcodec,libavcodec)
-
-$(call register-prebuilt-pkg-config-module,libavutil,libavutil)
